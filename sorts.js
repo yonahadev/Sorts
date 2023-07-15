2fdc9cf9a42c7dc68c2ab17116e216e8b1e6b162
tom <83908974+goat6@users.noreply.github.com>
2023-07-15 11:49:23 +0100

    coded merge sort

--- sorts.js
+++ sorts.js
@@ -1,38 +1,80 @@
-let unsortedData = [4, 2, 7, 6, 5, 10, 1]
-const insertionSort = (unsortedData) => {
+let list = [4, 2, 7, 6, 5, 10, 1]
+
+const insertionSort = (list) => {
   let sortedList = []
-   for (let i = 0; i < unsortedData.length; i++) {
+   for (let i = 0; i < list.length; i++) {
       if (sortedList.length === 0) {
-        sortedList.push(unsortedData[i])
+        sortedList.push(list[i])
       } else {
-        for (let j = 0; j < unsortedData.length; j++) {
-          if (unsortedData[i] > sortedList[j]) {
+        for (let j = 0; j < list.length; j++) {
+          if (list[i] > sortedList[j]) {
             continue
           } else {
-            sortedList.splice(j, 0, unsortedData[i])
+            sortedList.splice(j, 0, list[i])
             break
             }
             }
           }
         }
         return sortedList
-      } 
+} 
 
-const bubbleSort = (unsortedData) => {
+const bubbleSort = (list) => {
   let temp = 0
   let flag = false
   while (flag == false ) {
     flag = true
-    for (let i = 0; i < unsortedData.length; i++) {
-      if (unsortedData[i] > unsortedData[i + 1]) {
+    for (let i = 0; i < list.length-1; i++) {
+      if (list[i] > list[i + 1]) {
         flag = false
-        temp = unsortedData[i + 1]
-        unsortedData[i + 1] = unsortedData[i]
-        unsortedData[i] = temp
+        temp = list[i + 1]
+        list[i + 1] = list[i]
+        list[i] = temp
       } 
     }
   }
-  return unsortedData
+  return list
 }
 
-console.log(bubbleSort(unsortedData))
+const mergeSort = (list) => {
+  const merge = (left, right, list) => {
+    let leftCounter = 0
+    let rightCounter = 0
+    let listIndex = 0
+    while (leftCounter < left.length && rightCounter < right.length) { //keeps going until either list has finished being iterated
+      if (left[leftCounter] <= right[rightCounter]) {
+        list[listIndex] = left[leftCounter]
+        leftCounter++
+      } else {
+        list[listIndex] = right[rightCounter]
+        rightCounter++
+      }
+      listIndex++
+    }
+    //pust the rest of the remaining list into list
+    while (leftCounter < left.length) {
+      list[listIndex] = left[leftCounter]
+      leftCounter++
+      listIndex++ 
+    }
+    while (rightCounter < right.length) {
+      list[listIndex] = right[rightCounter]
+      rightCounter++
+      listIndex++
+    }
+    console.log(list)
+  }
+    
+  if (list.length === 1) return //base case that checks sub lists on each call
+  const middle = Math.ceil(list.length / 2)
+  const leftSide = list.slice(0, middle)
+  const rightSide = list.slice(middle)
+  mergeSort(leftSide)
+  mergeSort(rightSide)
+  merge(leftSide, rightSide, list) 
+
+
+  return list
+}
+
+console.log(mergeSort(list))
